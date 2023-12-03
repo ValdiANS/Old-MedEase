2610cc7b36c1f4807d67b94f7d1546c1fe1f100f
Rivaldi A N S <66946020+ValdiANS@users.noreply.github.com>
2023-12-03 20:13:38 +0700

    Revert "Dev rivaldi/detail screen"

--- app/src/main/java/com/myapplication/medease/MedEaseApp.kt
+++
@@ -1,9 +0,0 @@
-package com.myapplication.medease
-
-import androidx.compose.runtime.Composable
-import com.myapplication.medease.ui.screens.home.HomeScreen
-
-@Composable
-fun MedEaseApp() {
-    HomeScreen()
-}
